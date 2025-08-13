4036b1a0ceed72fb9331f17ba1af5634511db599
bianca principi <b.principi@itecriocuarto.org.ar>
2025-08-13 10:47:55 -0300

    accounts completos

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    class Roles(models.TextChoices):
+        ADMIN = "ADMIN", "Administrador"
+        AGENT = "AGENT", "Agente"
+        CUSTOMER = "CUSTOMER", "Usuario"
+
+    role = models.CharField(max_length=20, choices=Roles.choices, default=Roles.CUSTOMER)
+
+    @property
+    def is_staff_like(self):
+        return self.is_staff or self.role in {self.Roles.ADMIN, self.Roles.AGENT}
